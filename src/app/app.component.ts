e96c69d189e4da5ae09e8f28b91fdb0cd6e2f524
mehdisellami <mehdisellami9@gmail.com>
2022-02-08 13:11:57 +0100

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'libary';
+
+  form: FormGroup;
+
+  TypeFC:FormControl
+  LangueFC:FormControl
+  CategorieFC:FormControl
+  NoteFC:FormControl
+
+  livre:any
+  dvd:any
+  francais:any
+  anglais:any
+  Litteraturefrancaise:any
+  Litteratureetrangere:any
+
+
+
+
+
+
+  constructor(fb:FormBuilder){
+    this.form = fb.group({
+      'type': this.TypeFC,
+      'categorie':this.CategorieFC,
+      'langue':this.LangueFC,
+      'note':this.NoteFC
+    });
+
+  }
+
+
+ngOnInit(){
+
+  this.TypeFC= new FormControl('');
+  this.LangueFC= new FormControl('');
+  this.CategorieFC= new FormControl('');
+  this.NoteFC= new FormControl('');
+
+}
+
+
+onSubmit(){
+  console.log(this.form)
+}
 }
